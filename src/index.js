e50c3c5045bd291c236729f27350f6cdabff034d
Emma Kincaid <emmakincaid@Emmas-MacBook-Air.local>
2021-06-28 16:33:27 +0100

    Movie Details Page rendering

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MovieDetailsPage from './pages/movieDetailsPage'
 import ReactDOM from "react-dom";
 import HomePage from "./pages/homePage";
 
@@ -83,12 +84,20 @@ const sample = {
   vote_count: 9692,
 };
 
+const images = [
+    "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+    "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+    "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+    "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+  ]
+  
+
 const movies = [sample, sample, sample, sample, sample, sample, sample];
 
 const App = () => {
-  return (
-      <HomePage movies={movies} />
-  );
-};
+    return (
+        <MovieDetailsPage movie={sample} images={images} />
+        );
+  };
 
 ReactDOM.render(<App />, document.getElementById("root"));
