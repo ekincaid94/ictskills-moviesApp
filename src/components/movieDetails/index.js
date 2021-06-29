3bf8c614b7b6306f01e10ef8f5400bf537f8bce8
Emma Kincaid <emmakincaid@Emmas-MacBook-Air.local>
2021-06-29 12:00:52 +0100

    Change site header

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";import React from "react";
+import React, { useState} from "react";
 import Chip from "@material-ui/core/Chip";
 import Paper from "@material-ui/core/Paper";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
